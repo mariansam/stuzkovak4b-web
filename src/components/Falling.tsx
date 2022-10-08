a478850a35f3981e91b6718f1e6bd3038fb95edb
Marian Šámal <margotka000@gmail.com>
2022-10-08 21:46:32 +0200

    Add waterfall 2 ????

--- src/components/Falling.tsx
+++ src/components/Falling.tsx
@@ -1,14 +1,69 @@
-import React, { useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
-const SIZE = 48;
+export const WIDTH = 48;
+export const HEIGHT = WIDTH * 1.9;
 
-const Falling: React.FC = () => {
-    const maxX = useMemo(() => {
-        return document.body.scrollHeight + SIZE;
+function randomIntFromInterval(min: number, max: number) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomFloatFromInterval(min: number, max: number) { // min and max included
+    return Math.random() * (max - min) + min;
+}
+
+function getInitialOffsetY(column: number, row: number) {
+    return randomIntFromInterval(-7 * HEIGHT, 0.5 * HEIGHT) - row * 6 * HEIGHT;
+}
+
+const map = (value: number, minOrig: number, maxOrig: number, minNew: number, maxNew: number) =>
+    (value - minOrig) * (maxNew - minNew) / (maxOrig - minOrig) + minNew;
+
+const map01 = (value: number, minNew: number, maxNew: number) =>
+    map(value, 0.001, 1, minNew, maxNew);
+
+type FallingProps = {
+    column: number;
+    row: number;
+};
+
+const Falling: React.FC<FallingProps> = (props) => {
+    const {
+        column,
+        row,
+    } = props;
+
+    // const [offsetY, setOffsetY] = useState(() => randomIntFromInterval(-7 * SIZE, 3 * SIZE));
+    const [offsetY, setOffsetY] = useState(() => getInitialOffsetY(column, row));
+    const [depth, setDepth] = useState(() => randomFloatFromInterval(0.001, 1));
+
+    const width = useMemo(() => {
+        return WIDTH + map01(depth, -30, 30);
+    }, [depth]);
+
+    const maxY = useMemo(() => {
+        return document.body.scrollHeight - HEIGHT;
+    }, []);
+
+    const offsetX = useMemo(() => {
+        return column * 150 + randomIntFromInterval(-0.4 * WIDTH, 1.3 * WIDTH);
+    }, []);
+
+    useEffect(() => {
+        const randomSpeed = map01(depth, 4, 25);
+
+        setInterval(() => {
+            setOffsetY(prev => {
+                if (prev + 1 > maxY)
+                    return getInitialOffsetY(column, row);
+                return prev + 1
+            });
+        }, randomSpeed);
     }, []);
 
     return (
-        <p>AAA</p>
+        <div className='absolute opacity-95' style={{top: offsetY, left: offsetX}}>
+            <img src="waterfall.png" width={width} />
+        </div>
     );
 };
 
--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,16 +1,22 @@
 import React, { useMemo } from 'react';
-import Falling from './Falling';
+import Falling, { HEIGHT } from './Falling';
 
 const Overlay: React.FC = () => {
-    const count = useMemo(() => {
+    const countWidth = useMemo(() => {
         console.log(document.body.scrollHeight);
-        return Math.floor(window.innerWidth / 60);
+        return Math.floor(window.innerWidth / 120);
     }, []);
 
+    const countHeight = useMemo(() => {
+        return Math.ceil(window.innerHeight / (2 * HEIGHT) + 1);
+    }, []);
+
+    console.log({countWidth, countHeight, HEIGHT});
+
     return (
-        <div className="absolute top-0 left-0 w-full h-full min-h-full bg-rose-500/50">
-            {[...Array(count)].map(() => (
-                <Falling />
+        <div className="flex flex-row pointer-events-none absolute top-0 left-0 w-full h-full min-h-full">
+            {[...Array(countWidth * countHeight)].map((value, index) => (
+                <Falling column={index % countWidth} row={Math.floor(index / countWidth)} />
             ))}
         </div>
     );
